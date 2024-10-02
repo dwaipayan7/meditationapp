01071e0389b8b8c6ed7ebbd6c27694ce8dc05f71
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-03 03:58:17 +0530

    update meditation_screen.dart

--- lib/feature/presentation/pages/meditation_screen.dart
+++ lib/feature/presentation/pages/meditation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:meditationapp/core/theme.dart';
 import 'package:meditationapp/feature/presentation/wigets/feeling_button.dart';
+
+import '../../../core/theme.dart';
 
 class MeditationScreen extends StatelessWidget {
   const MeditationScreen({super.key});
@@ -12,40 +13,45 @@ class MeditationScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
         leading: Image.asset('assets/menu_burger.png'),
-        actions: [
+        actions: const [
           CircleAvatar(
             backgroundImage: AssetImage('assets/profile.png'),
           ),
           SizedBox(width: 16,)
         ],
       ),
-      body:  SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Text("Welcome back!", style: TextStyle(
-                color: Colors.black, fontSize: 34, fontWeight: FontWeight.bold
+      // backgroundColor: Colors.red,
+      body: SingleChildScrollView(
+          child: Column(
+            children: [
+               Text("Welcome back!", style: TextStyle(
+                  color: Colors.black, fontSize: 34, fontWeight: FontWeight.bold
+                ),),
+        
+              SizedBox(height: 32,),
+              Text("How are you feeling today? ", style: TextStyle(
+                color: Colors.black87, fontWeight: FontWeight.w500, fontSize: 16
+              ),),
+              SizedBox(height: 16,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FeelingButton(label: "Happy", image: 'assets/happy.png', color: DefaultColors.pink),
+                  FeelingButton(label: "Calm", image: 'assets/calm.png', color: DefaultColors.purple),
+                  FeelingButton(label: "Relax", image: 'assets/relax.png', color: DefaultColors.orange),
+                  FeelingButton(label: "Focus", image: 'assets/focus.png', color: DefaultColors.lightteal),
+                ],
               ),
-              ),
-            ),
-            SizedBox(height: 32,),
-            Text("How are you feeling today? ", style: TextStyle(
-              color: Colors.black87, fontWeight: FontWeight.w500, fontSize: 16
-            ),),
-            SizedBox(height: 16,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FeelingButton(label: "Happy", image: 'assets/happy.png', color: DefaultColors.pink),
-                FeelingButton(label: "Calm", image: 'assets/calm.png', color: DefaultColors.purple),
-                FeelingButton(label: "Relax", image: 'assets/relax.png', color: DefaultColors.orange),
-                FeelingButton(label: "Focus", image: 'assets/focus.png', color: DefaultColors.lightteal),
-              ],
-            )
-          ],
+              SizedBox(height: 10,),
+              Text("Today's Task",
+              style: TextStyle(
+                color: Colors.black, fontSize: 18,
+                fontWeight: FontWeight.w500
+              ),),
+
+            ],
+          ),
         ),
-      ),
     );
   }
 }
---
+++ lib/feature/presentation/wigets/task_card.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+//TODO task_card
