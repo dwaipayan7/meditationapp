4a7e055cd1c268116aae5d07cebf06fcbd1c218e
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-03 03:47:04 +0530

    update meditation_screen.dart

--- lib/feature/presentation/pages/meditation_screen.dart
+++ lib/feature/presentation/pages/meditation_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meditationapp/core/theme.dart';
+import 'package:meditationapp/feature/presentation/wigets/feeling_button.dart';
 
 class MeditationScreen extends StatelessWidget {
   const MeditationScreen({super.key});
@@ -9,7 +11,7 @@ class MeditationScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: Icon(Icons.menu_sharp),
+        leading: Image.asset('assets/menu_burger.png'),
         actions: [
           CircleAvatar(
             backgroundImage: AssetImage('assets/profile.png'),
@@ -26,6 +28,20 @@ class MeditationScreen extends StatelessWidget {
                 color: Colors.black, fontSize: 34, fontWeight: FontWeight.bold
               ),
               ),
+            ),
+            SizedBox(height: 32,),
+            Text("How are you feeling today? ", style: TextStyle(
+              color: Colors.black87, fontWeight: FontWeight.w500, fontSize: 16
+            ),),
+            SizedBox(height: 16,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FeelingButton(label: "Happy", image: 'assets/happy.png', color: DefaultColors.pink),
+                FeelingButton(label: "Calm", image: 'assets/calm.png', color: DefaultColors.purple),
+                FeelingButton(label: "Relax", image: 'assets/relax.png', color: DefaultColors.orange),
+                FeelingButton(label: "Focus", image: 'assets/focus.png', color: DefaultColors.lightteal),
+              ],
             )
           ],
         ),
