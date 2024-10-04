bbd4973d2e43a5e4c31496e8549e0fd025545497
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-04 16:38:50 +0530

    Update properly

--- lib/feature/presentation/HomePage/home_page.dart
+++ lib/feature/presentation/HomePage/home_page.dart
@@ -2,23 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meditationapp/feature/presentation/bottom_Nav_bar/bloc/navigation_bloc.dart';
 import 'package:meditationapp/feature/presentation/bottom_Nav_bar/widgets/bottom_nav_bar.dart';
+import 'package:meditationapp/feature/presentation/pages/meditation_screen.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   final List<Widget> pages = [
-    Scaffold(body: Center(child: Text('test 1', style: TextStyle(color: Colors.red),),),),
-    Scaffold(body: Center(child: Text('test 2', style: TextStyle(color: Colors.red),),),),
-    Scaffold(body: Center(child: Text('test 3', style: TextStyle(color: Colors.red),),),),
+    const MeditationScreen(),
+    Scaffold(body: Center(child: Text('Songs Screen'))),
   ];
+
+  BottomNavigationBarItem createBottomNavItem(
+      String assetName, bool isActive, BuildContext context) {
+    return BottomNavigationBarItem(
+      icon: Image.asset(
+        assetName,
+        color: isActive
+            ? Theme.of(context).primaryColor
+            : Theme.of(context).focusColor,
+      ),
+      label: '',
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final List<BottomNavigationBarItem> bottomNavItems = [
-      BottomNavigationBarItem(icon: Image.asset('assets/menu_home.png'), label: ''),
-      BottomNavigationBarItem(icon: Image.asset('assets/menu_songs.png'), label: ''),
-    ];
-
     return Scaffold(
       body: BlocBuilder<NavigationBloc, NavigationState>(
           builder: (context, state) {
@@ -26,8 +34,7 @@ class HomePage extends StatelessWidget {
               return pages[state.index];
             }
             return pages[0]; // Default to first page if no state change
-          }
-      ),
+          }),
       bottomNavigationBar: BlocBuilder<NavigationBloc, NavigationState>(
           builder: (context, state) {
             int currentIndex = 0;
@@ -35,11 +42,13 @@ class HomePage extends StatelessWidget {
               currentIndex = state.index;
             }
             return BottomNavBar(
-              items: bottomNavItems,
+              items: [
+                createBottomNavItem('assets/menu_home.png', currentIndex == 0, context),
+                createBottomNavItem('assets/menu_songs.png', currentIndex == 1, context),
+              ],
               currentIndex: currentIndex,
             );
-          }
-      ),
+          }),
     );
   }
 }
