1b63cde143e9f14dbffb3aa34d729a89faf7bdf3
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-06 00:07:28 +0530

    updated

---
+++ server/application/interfaces/usecases/GetSongs.js
@@ -0,0 +1,17 @@
+const UseCaseInterface = require("../UseCaseInterface");
+const Song = require('../../../domain/entities/Song')
+
+class GetSongs extends UseCaseInterface{
+    async execute(){
+        const songRows = await getAllSongs();
+        return songRows.map(song => new Song({
+
+            id: song.id,
+            title: song.title,
+            author: song.author,
+            songLink: song.songLink
+        }));
+    }
+}
+
+module.exports = GetSongs;
